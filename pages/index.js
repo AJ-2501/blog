3f365b6bbf3ef99ba1aa4144053995d504c7961b
AJ Cruz <ajdcruz.student@ua.edu.ph>
2026-08-18 11:46:22 +0800

    Change description

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I’m Alexander Jude Cruz. I’m an I.T student at The University of The Assumption.</p>
+        <p>Hi, I’m Alexander Jude Cruz. My hobbies are playing video games, playing basketball, and cooking. My favorite song is “Wonderwall” by Oasis. My favorite foods are laing and beef kaldereta. In five years, I see myself building my career and working in a field related to the course I have taken.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
